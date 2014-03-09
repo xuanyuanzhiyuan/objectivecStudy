7b7b5554263b8eb2142b267e1148efb8ce87892e
xuanyuanzhiyuan <xuanyuanzhiyuan@gmail.com>
2014-03-09 11:05:25 +0800

    init commint from local git repo

---
+++ ObjectivecStudy/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectivecStudy
+//
+//  Created by  Jinyanhua on 14-3-9.
+//  Copyright (c) 2014年 com.xysoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
