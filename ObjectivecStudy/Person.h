32a6eba2fb8212462f3b994112da125117dd0838
xuanyuanzhiyuan <xuanyuanzhiyuan@gmail.com>
2014-03-09 11:10:12 +0800

    add person class.

---
+++ ObjectivecStudy/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  ObjectivecStudy
+//
+//  Created by  Jinyanhua on 14-3-9.
+//  Copyright (c) 2014年 com.xysoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ ObjectivecStudy/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  ObjectivecStudy
+//
+//  Created by  Jinyanhua on 14-3-9.
+//  Copyright (c) 2014年 com.xysoft. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
